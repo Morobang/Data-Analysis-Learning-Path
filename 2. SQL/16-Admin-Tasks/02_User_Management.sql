2ff54a92a2f7c37fd53a068b8616672b1e1e25e7
Morobang <143880454+Morobang@users.noreply.github.com>
2025-05-20 18:44:04 +0200

    adding more content to user management

--- 2. SQL/16-Admin-Tasks/02_User_Management.sql
+++ 2. SQL/16-Admin-Tasks/02_User_Management.sql
@@ -1,10 +1,42 @@
+-- Grant SELECT on all tables in a schema
+GRANT SELECT ON SCHEMA::Sales TO AnalystUser;
 
--- User Management (GRANT, REVOKE)
--- Managing user permissions is essential for database security.
+-- Grant ALL permissions on a specific table
+GRANT ALL ON Orders TO AdminUser;
 
--- Example: Granting permissions to a user
-GRANT SELECT, INSERT ON Person.Person TO UserName;
+-- Grant UPDATE and DELETE permissions on a table
+GRANT UPDATE, DELETE ON Sales.Orders TO SalesManager;
 
--- Example: Revoking permissions from a user
-REVOKE INSERT ON Person.Person FROM UserName;
-    
+-- Grant EXECUTE on a stored procedure
+GRANT EXECUTE ON OBJECT::dbo.usp_GetCustomerData TO AppUser;
+
+-- Grant CREATE TABLE permission in a database
+GRANT CREATE TABLE TO DeveloperUser;
+
+-- Grant CONTROL on a schema (gives full control over all objects in it)
+GRANT CONTROL ON SCHEMA::Finance TO FinanceAdmin;
+
+---------------------------------------------------------------------------------------------------
+
+
+
+-- Revoke SELECT permission from a user
+REVOKE SELECT ON Customers FROM InternUser;
+
+-- Revoke all permissions on a table
+REVOKE ALL ON Products FROM ContractorUser;
+
+-- Revoke EXECUTE on a stored procedure
+REVOKE EXECUTE ON OBJECT::dbo.usp_UpdateInventory FROM AppUser;
+
+-- Revoke CONTROL on a schema
+REVOKE CONTROL ON SCHEMA::Finance FROM FinanceAdmin;
+
+
+
+-----------------------------------------------------------------
+
+
+CREATE ROLE SalesReaders;
+GRANT SELECT ON Sales.Orders TO SalesReaders;
+EXEC sp_addrolemember 'SalesReaders', 'User1';
